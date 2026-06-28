925bfa0d188bfff4998b124da0e9ebe97ce71627
minkee009 <minkee5004@gmail.com>
2026-06-28 21:00:41 +0900

    initialize git

---
+++ YWMA/src/Program.cs
@@ -0,0 +1,36 @@
+﻿using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using System;
+
+namespace YWMA
+{
+    public class Program
+    {
+        private static IWindow window;
+        private static GL gl;
+
+        public static void Main(string[] args)
+        {
+            var options = WindowOptions.Default;
+            options.Size = new Silk.NET.Maths.Vector2D<int>(800, 600);
+            options.Title = "YWMA - Yume Wo Miru Arisu";
+
+            window = Window.Create(options);
+            window.Load += OnLoad;
+            window.Render += OnRender;
+            window.Run();
+        }
+
+        private static void OnLoad()
+        {
+            gl = GL.GetApi(window);
+            // Initialize OpenGL resources here
+        }
+
+        private static void OnRender(double deltaTime)
+        {
+            gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+            // Render your scene here
+        }
+    }
+}
