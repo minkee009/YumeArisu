fbc168dd35545b02afc0fa3d4eece5176c732de2
minkee009 <minkee5004@gmail.com>
2026-06-30 12:40:53 +0900

    Clean File Directory

--- YWMA/src/Program.cs
+++ YWMA/src/Program.cs
@@ -2,35 +2,34 @@
 using Silk.NET.Windowing;
 using System;
 
-namespace YWMA
+namespace YWMA.Core;
+
+public class Program
 {
-    public class Program
+    private static IWindow window;
+    private static GL gl;
+
+    public static void Main(string[] args)
     {
-        private static IWindow window;
-        private static GL gl;
+        var options = WindowOptions.Default;
+        options.Size = new Silk.NET.Maths.Vector2D<int>(800, 600);
+        options.Title = "YWMA - Yume Wo Miru Arisu";
 
-        public static void Main(string[] args)
-        {
-            var options = WindowOptions.Default;
-            options.Size = new Silk.NET.Maths.Vector2D<int>(800, 600);
-            options.Title = "YWMA - Yume Wo Miru Arisu";
+        window = Window.Create(options);
+        window.Load += OnLoad;
+        window.Render += OnRender;
+        window.Run();
+    }
 
-            window = Window.Create(options);
-            window.Load += OnLoad;
-            window.Render += OnRender;
-            window.Run();
-        }
+    private static void OnLoad()
+    {
+        gl = GL.GetApi(window);
+        // Initialize OpenGL resources here
+    }
 
-        private static void OnLoad()
-        {
-            gl = GL.GetApi(window);
-            // Initialize OpenGL resources here
-        }
-
-        private static void OnRender(double deltaTime)
-        {
-            gl.Clear((uint)ClearBufferMask.ColorBufferBit);
-            // Render your scene here
-        }
+    private static void OnRender(double deltaTime)
+    {
+        gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+        // Render your scene here
     }
 }
